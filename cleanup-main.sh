450ab9f0dbce5aaf1171c3ae7160f224e4a918fd
Jeremy Morgan <153001285+jeremy-morgan-deque@users.noreply.github.com>
2026-02-18 17:38:47 -0500

    added cleanup scripts

---
+++ cleanup-main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Reset all tracked files to their last committed state
+git checkout -- .
+
+# Remove all untracked files and directories (but preserve .vscode)
+git clean -fd
+
+# Explicitly remove common build artifacts to be thorough (preserving .vscode)
+rm -rf target test-output .DS_Store
+
+# Reset any staged changes
+git reset --hard HEAD
+
+# Remove axe linter hooks
+#rm .git/hooks/pre-commit-axe-linter
+#rm .git/hooks/pre-commit
+
+# Switch to main branch
+git checkout main
